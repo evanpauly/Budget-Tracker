5035d6f4f43a9706a1dd7309988e66ace2a72d53
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-05-08 13:35:01 -0500

    offline functionality testing

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,7 +6,7 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    // create an object store (table) called `new_tracker`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_tracker', { autoIncrement: true });
     };
 
@@ -37,3 +37,49 @@ function saveRecord(record) {
     // add record to your store with add method
     trackerObjectStore.add(record);
     }
+
+    function uploadTracker() {
+        // open a transaction on your db
+        const transaction = db.transaction(['new_tracker'], 'readwrite');
+    
+        // access your object store
+        const trackerObjectStore = transaction.objectStore('new_tracker');
+    
+        // get all records from store and set to a variable
+        const getAll = trackerObjectStore.getAll();
+    
+        // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+    fetch('/api/', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(serverResponse => {
+        if (serverResponse.message) {
+            throw new Error(serverResponse);
+        }
+          // open one more transaction
+        const transaction = db.transaction(['new_tracker'], 'readwrite');
+          // access the new_tracker object store
+        const trackerObjectStore = transaction.objectStore('new_tracker');
+          // clear all items in your store
+        trackerObjectStore.clear();
+
+        alert('All saved information has been submitted!');
+        })
+        .catch(err => {
+        console.log(err);
+        });
+    }
+    };
+    }
+
+    // listen for app coming back online
+window.addEventListener('online', uploadTracker);
