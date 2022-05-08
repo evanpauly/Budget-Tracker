a6cc320bc2880b86ce232801d1aef8bdfa750d08
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-05-08 17:37:35 -0500

    offline data storage

--- public/js/idb.js
+++ public/js/idb.js
@@ -26,7 +26,7 @@ request.onerror = function(event) {
     console.log(event.target.errorCode);
     };
 
-// This function will be executed if we attempt to submit a new budget and there's no internet connection
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_tracker'], 'readwrite');
@@ -52,7 +52,7 @@ function saveRecord(record) {
     getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-    fetch('/api/', {
+    fetch('/api/transaction', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
